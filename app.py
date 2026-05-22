d4056e49e20468bbdc74b2ebbb3e29f2c36095bc
SanthoshM-2004 <m.santhosh.200411@gmail.com>
2026-05-22 17:20:19 +0530

    Fixed production contact route

--- app.py
+++ app.py
@@ -59,17 +59,11 @@ Message:
 
         msg.attach(MIMEText(body, "plain"))
 
-        server = smtplib.SMTP(
-            "smtp.gmail.com",
-            587,
-            timeout=10
+        server = smtplib.SMTP_SSL(
+        "smtp.gmail.com",
+        465,
+        timeout=10
         )
-
-        server.ehlo()
-
-        server.starttls()
-
-        server.ehlo()
 
         server.login(
             EMAIL_USER,
